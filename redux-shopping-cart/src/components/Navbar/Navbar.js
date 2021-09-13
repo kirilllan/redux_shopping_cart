f16203ee71628862d180021d3cc20418735d678d
kirilllan <s2100138@edu.bc.fi>
2021-09-13 14:30:59 +0300

    code in product&products

--- redux-shopping-cart/src/components/Navbar/Navbar.js
+++ redux-shopping-cart/src/components/Navbar/Navbar.js
@@ -10,9 +10,8 @@ export default function Navbar() {
       </Link>
       <Link to="/cart">
         <div className={styles.navbar__cart}>
-          <h3>className={styles.cart__title}Cart</h3>
+          <h3 className={styles.cart__title}>Cart</h3>
           <img className={styles.cart__image}
-            className={styles.cart__image}
             src="https://media.istockphoto.com/vectors/shopping-cart-icon-isolated-on-white-background-vector-id1206806317?k=20&m=1206806317&s=612x612&w=0&h=waK8qOHV2Fgz2ntEWHWBQtXpNDAQ_wdhd4tkTUz6tfE="
             alt="image of shopping cart"
           />
--- redux-shopping-cart/src/components/Products/Products.js
+++ redux-shopping-cart/src/components/Products/Products.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from './Products.module.css'
+import Product from './Product/Product'
 
 export default function Products() {
   return (
-    <div>
-
+    <div className={styles.products}>
+      <Product />
     </div>
   )
 }
