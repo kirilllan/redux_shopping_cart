4015711e528737f8c37845d4b77b4769fe7a7ab5
kirilllan <s2100138@edu.bc.fi>
2021-09-14 11:26:02 +0300

    add all shopping-types as exports

--- redux-shopping-cart/src/redux/rootReducer.js
+++ redux-shopping-cart/src/redux/rootReducer.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import shopReducer from './Shopping/shopping-reducer'
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+  shop: shopReducer
+})
 
 export default rootReducer
