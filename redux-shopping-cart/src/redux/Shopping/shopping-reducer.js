2c4be0e274228068f56e9cc8e69657c6172be242
kirilllan <s2100138@edu.bc.fi>
2021-09-14 12:38:42 +0300

    adding to cart functionality

--- redux-shopping-cart/src/redux/Shopping/shopping-reducer.js
+++ redux-shopping-cart/src/redux/Shopping/shopping-reducer.js
@@ -8,7 +8,8 @@ const initialState = {
       description: 'description1',
       price: 1,
       image: 'grid1.png'
-    }
+    },
+
   ], //{id, title, descr, price, img}
   cart: [], //{id, title, descr, price, img, QUANTITY}
   currentItem: null
@@ -16,7 +17,14 @@ const initialState = {
 
 const shopReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_TO_CART: return {}
+    case actionTypes.ADD_TO_CART:
+      const item = state.products.find(p => p.id === action.payload.id)
+      const inCart = state.cart.find(item => item.id === action.payload.id) ? true : false
+      return {
+        ...state,
+        cart: inCart ? state.cart.map(item => item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item)
+          : [...state.cart, { ...item, quantity: 1 }]
+      }
     case actionTypes.REMOVE_FROM_CART: return {}
     case actionTypes.ADJUST_QUANTITY: return {}
     case actionTypes.LOAD_CURRENT_ITEM: return {}
