90a0e8cf55f7731efb72da7c97fb5cf4a3d12b2e
kirilllan <s2100138@edu.bc.fi>
2021-09-14 11:44:03 +0300

    base for shopping-reducer built

---
+++ redux-shopping-cart/src/redux/Shopping/shopping-reducer.js
@@ -0,0 +1,19 @@
+import * as actionTypes from './shopping-types'
+
+const initialState = {
+  products: [], //{id, title, descr, price, img}
+  cart: [], //{id, title, descr, price, img, QUANTITY}
+  currentItem: null
+}
+
+const shopReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_TO_CART: return {}
+    case actionTypes.REMOVE_FROM_CART: return {}
+    case actionTypes.ADJUST_QUANTITY: return {}
+    case actionTypes.LOAD_CURRENT_ITEM: return {}
+    default: return state
+  }
+}
+
+export default shopReducer
