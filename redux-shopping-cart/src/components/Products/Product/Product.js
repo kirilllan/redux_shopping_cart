117f0c1a6c09cb1d0b1160e4ac3790e694639b9b
kirilllan <s2100138@edu.bc.fi>
2021-09-14 12:10:09 +0300

    Product.js should be rendering dynamically

--- redux-shopping-cart/src/components/Products/Product/Product.js
+++ redux-shopping-cart/src/components/Products/Product/Product.js
@@ -3,10 +3,29 @@ import { Link } from 'react-router-dom'
 import styles from './Product.module.css'
 
 
-export default function Product() {
+export default function Product({ productData }) {
   return (
-    <div>
+    <div className={styles.product}>
+      <img className={styles.product__image}
+        src={productData.image}
+        alt={productData.title} />
 
+      <div className={styles.product__details}>
+        <p className={styles.details__title}></p>
+        <p className={styles.details__desc}>
+          {productData.description}
+        </p>
+        <p className={styles.details__price}>
+          $ {productData.price}
+        </p>
+      </div>
+
+      <div className={styles.product__buttons}>
+        <Link to={'/product/someId'}>
+          <button className={`${styles.button__btn} ${styles.button__view}`}>View item</button>
+        </Link>
+        <button className={`${styles.buttons__btn} ${styles.buttons__add}`}>Add to cart</button>
+      </div>
     </div>
   )
 }
