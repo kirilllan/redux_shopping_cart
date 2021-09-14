3d1e01e3f1a1ac5a5667172c812957806a28cc57
kirilllan <s2100138@edu.bc.fi>
2021-09-14 11:57:19 +0300

    mapstatetoprops, mappinr Product components

--- redux-shopping-cart/src/components/Products/Products.js
+++ redux-shopping-cart/src/components/Products/Products.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import styles from './Products.module.css'
 import Product from './Product/Product'
+import { connect } from 'react-redux'
 
-export default function Products() {
+function Products({ products }) {
   return (
     <div className={styles.products}>
-      <Product />
+      {products.map(prod => < Product key={prod.id} productData={prod} />)}
     </div>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    products: state.shop.products
+  }
+}
+
+export default connect(mapStateToProps)(Products)
