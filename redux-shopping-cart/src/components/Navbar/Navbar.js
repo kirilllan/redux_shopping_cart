278c83216e6975b6218092aa0dfac83af141fd28
kirilllan <s2100138@edu.bc.fi>
2021-09-19 12:05:49 +0300

    inc/dec cart items N in Nav

--- redux-shopping-cart/src/components/Navbar/Navbar.js
+++ redux-shopping-cart/src/components/Navbar/Navbar.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
 import styles from "./Navbar.module.css"
+import { connect } from 'react-redux'
 
-export default function Navbar() {
+function Navbar({ cart }) {
+
+  const [cartCount, setCartCount] = useState(0)
+  useEffect(() => {
+    let count = 0
+    cart.forEach(item => {
+      count += item.quantity
+    })
+    setCartCount(count)
+  }, [cart, cartCount])
+
   return (
     <div className={styles.navbar}>
       <Link to="/">
@@ -15,10 +26,18 @@ export default function Navbar() {
             src="https://media.istockphoto.com/vectors/shopping-cart-icon-isolated-on-white-background-vector-id1206806317?k=20&m=1206806317&s=612x612&w=0&h=waK8qOHV2Fgz2ntEWHWBQtXpNDAQ_wdhd4tkTUz6tfE="
             alt="image of shopping cart"
           />
-          <div className={styles.cart__counter}>1</div>
+          <div className={styles.cart__counter}>{cartCount}</div>
         </div>
       </Link>
 
     </div>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    cart: state.shop.cart
+  }
+}
+
+export default connect(mapStateToProps)(Navbar)
