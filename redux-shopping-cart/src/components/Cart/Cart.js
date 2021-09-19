4fd2c9f0288cc5fc592550f07a16f5afbf6cc822
kirilllan <s2100138@edu.bc.fi>
2021-09-19 12:15:23 +0300

    base for Cart.js

--- redux-shopping-cart/src/components/Cart/Cart.js
+++ redux-shopping-cart/src/components/Cart/Cart.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import CartItem from './CartItem/CartItem'
+import styles from './Cart.module.css'
 
 export default function Cart() {
   return (
-    <div>
-
+    <div className={styles.cart}>
+      <div className={styles.cart__items}>
+        <CartItem />
+      </div>
+      <div className={styles.cart__summary}>
+        <h4 className={styles.summary__title}>Cart summary</h4>
+        <div className={styles.summary__price}>
+          <span>TOTAL: (1 items)</span>
+          <span>$ 15.00</span>
+        </div>
+        <button className={styles.summary__checkoutBtn}>Proceed to checkout</button>
+      </div>
     </div>
   )
 }
