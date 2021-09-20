3ff5c024b8b5c7c7735e1be3a49259c7564b7055
kirilllan <s2100138@edu.bc.fi>
2021-09-20 12:23:48 +0300

    fix refreshing error in App

--- redux-shopping-cart/src/App.js
+++ redux-shopping-cart/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import React from 'react'
 import {
-  BrowserRouter as Router, Switch, Route
+  BrowserRouter as Router, Switch, Route, Redirect
 } from 'react-router-dom'
 
 import Navbar from "./components/Navbar/Navbar"
 import Products from './components/Products/Products'
 import Cart from './components/Cart/Cart'
 import SingleItem from './components/SingleItem/SingleItem'
+import { connect } from 'react-redux'
 
-function App() {
+function App({ currentItem }) {
   return (
     <Router>
       <div className="App">
@@ -17,11 +18,17 @@ function App() {
         <Switch>
           <Route exact path="/" component={Products} />
           <Route exact path="/cart" component={Cart} />
-          <Route exact path="/product/:id" component={SingleItem} />
+          {!currentItem ? (<Redirect to="/" />) : (<Route exact path="/product/:id" component={SingleItem} />)}
         </Switch>
       </div>
     </Router>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    currentItem: state.shop.currentItem
+  }
+}
+
+export default connect(mapStateToProps)(App);
