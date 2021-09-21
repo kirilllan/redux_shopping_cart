c0b6bb9771f2a63ffacc589c8ebdc3e3656080b1
kirilllan <s2100138@edu.bc.fi>
2021-09-21 13:36:10 +0300

    product.js model

--- mern-shopping-cart/backend/server.js
+++ mern-shopping-cart/backend/server.js
@@ -3,3 +3,6 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+
+const connectDB = require('./config/db')
+connectDB()
