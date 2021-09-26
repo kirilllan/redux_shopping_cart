0a2ac6cf74a74314b6ae423c74bb5b90efe32a8e
kirilllan <s2100138@edu.bc.fi>
2021-09-26 15:47:43 +0300

    new installations

--- mern-shopping-cart/README.md
+++ mern-shopping-cart/README.md
@@ -3,3 +3,4 @@
 ### npm i --save-dev nodemon concurrently
 ### -> cd frontend -> npx create-react-app .
 ### node backend/server.js
+### frontend folder: npm i axios react-router-dom redux react-redux redux-thunk redux-devtools-extension
---
+++ mern-shopping-cart/frontend/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">
+        <h2>MERN shopping cart</h2>
+      </div>
+      <ul className="navbar__links">
+        <li>
+          <Link>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
