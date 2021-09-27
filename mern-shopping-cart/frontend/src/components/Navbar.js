35f9a37fee4742fa934861adde6c2b3875a9d31b
kirilllan <s2100138@edu.bc.fi>
2021-09-27 15:12:07 +0300

    navbar links styling & badge

--- mern-shopping-cart/frontend/src/components/Navbar.js
+++ mern-shopping-cart/frontend/src/components/Navbar.js
@@ -9,10 +9,12 @@ function Navbar() {
       </div>
       <ul className="navbar__links">
         <li>
-          <Link to="/cart">
+          <Link to="/cart" className="cart__link">
             <i className="fas fa-shopping-cart" />
-            Cart
-            <span className="cartlogo__badge">0</span>
+            <span>
+              Cart
+              <span className="cartlogo__badge">0</span>
+            </span>
           </Link>
         </li>
         <li>
