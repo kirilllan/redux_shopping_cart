96599ec0da6e9901a305abdb44399f626bb62f6d
kirilllan <s2100138@edu.bc.fi>
2021-09-29 17:00:56 +0300

    left-info

--- mern-shopping-cart/frontend/src/screens/ProductScreen.js
+++ mern-shopping-cart/frontend/src/screens/ProductScreen.js
@@ -7,6 +7,11 @@ function ProductScreen() {
         <div className="left__image">
           <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="product name" />
         </div>
+        <div className="left__info">
+          <p className="left__name">Product 1</p>
+          <p>Price: $599.99</p>
+          <p>Decription: lalalala this is a description here yes it is, here it is, hehe</p>
+        </div>
       </div>
       <div className="productscreen__right">
 
