c4f553bdc3c55771cf3b16d2d3bb3216f95d1375
kirilllan <s2100138@edu.bc.fi>
2021-10-07 22:55:13 +0300

    store file done

--- mern-shopping-cart/frontend/src/redux/store.js
+++ mern-shopping-cart/frontend/src/redux/store.js
@@ -2,3 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+const reducer = combineReducers({
+
+})
+
+const middleWare = [thunk]
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleWare)))
+
+export default store
