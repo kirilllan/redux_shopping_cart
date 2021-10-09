0aa60b4ef4103602b0c53fd34b6df52052e053ae
kirilllan <s2100138@edu.bc.fi>
2021-10-09 13:20:33 +0300

    actionType add_to_cart logic

--- mern-shopping-cart/frontend/src/redux/reducers/cartReducers.js
+++ mern-shopping-cart/frontend/src/redux/reducers/cartReducers.js
@@ -2,6 +2,21 @@ import * as actionTypes from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
+    case actionTypes.ADD_TO_CART:
+      const item = action.payload
+      const existItem = state.cartItems.find(x => x.product === item.product)
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map(x => x.product === existingItem.product ? item : x)
+        }
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item]
+        }
+      }
 
+    default: return state
   }
 }
