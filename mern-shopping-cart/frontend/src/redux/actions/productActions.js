7dfaa42cd56666e4bb52a47726dfcfc84111f4c7
kirilllan <s2100138@edu.bc.fi>
2021-10-11 12:35:00 +0300

    product details and get product details

--- mern-shopping-cart/frontend/src/redux/actions/productActions.js
+++ mern-shopping-cart/frontend/src/redux/actions/productActions.js
@@ -17,3 +17,27 @@ export const getProducts = () => async (dispatch) => {
     })
   }
 }
+
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: actionTypes.GET_PRODUCTS_DETAILS_REQUEST })
+    const { data } = await axios.get(`api/products/${id}`)
+    dispatch({
+      type: actionTypes.GET_PRODUCTS_DETAILS_SUCCESS,
+      payload: data
+    })
+  } catch {
+    dispatch({
+      type: actionTypes.GET_PRODUCT_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    })
+  }
+}
+
+export const removeProductDetails = () => {
+  (dispatch) => {
+    dispatch({
+      type: actionTypes.GET_PRODUCT_DETAILS_RESET
+    })
+  }
+}
