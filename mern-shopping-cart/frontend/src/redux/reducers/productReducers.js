b872d023c90ba00db1a8c64fabead00a76bc9462
kirilllan <s2100138@edu.bc.fi>
2021-10-11 11:52:12 +0300

    whole func of getProductDetails

--- mern-shopping-cart/frontend/src/redux/reducers/productReducers.js
+++ mern-shopping-cart/frontend/src/redux/reducers/productReducers.js
@@ -12,6 +12,35 @@ export const getProductsReducer = (state = { products: [] }, action) => {
         loading: false,
         products: action.payload
       }
+    case actionTypes.GET_PRODUCTS_FAIL:
+      return {
+        loading: false,
+        error: action.payload
+      }
+    default: return state
+  }
+}
 
+export const getProductDetails = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case actionTypes.GET_PRODUCT_DETAILS_REQUEST:
+      return {
+        loading: true
+      }
+    case actionTypes.GET_PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload
+      }
+    case actionTypes.GET_PRODUCTS_FAIL:
+      return {
+        loading: false,
+        error: action.payload
+      }
+    case actionTypes.GET_PRODUCT_DETAILS_RESET:
+      return {
+        product: {}
+      }
+    default: return state;
   }
 }
