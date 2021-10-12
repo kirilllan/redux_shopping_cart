dada23c016288d52a655edfb0b120789bdd7e01e
kirilllan <s2100138@edu.bc.fi>
2021-10-12 15:46:39 +0300

    dispatching listProducts

--- mern-shopping-cart/frontend/src/screens/HomeScreen.js
+++ mern-shopping-cart/frontend/src/screens/HomeScreen.js
@@ -1,6 +1,18 @@
 import './HomeScreen.css'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Product from '../components/Product'
+import { getProducts as listProducts } from '../redux/actions/productActions'
 
 function HomeScreen() {
+  const dispatch = useDispatch()
+  const getProducts = useSelector(state => state.getProducts)
+  const { products, loading, error } = getProducts
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+
   return (
     <div className="homescreen">
       <h2 className="homescreen_title">Latest products</h2>
