63262e0dec77ea94c62dff0a48468c1c97266442
kirilllan <s2100138@edu.bc.fi>
2021-10-13 10:26:23 +0300

    ProductAction fixes

--- mern-shopping-cart/frontend/src/redux/actions/productActions.js
+++ mern-shopping-cart/frontend/src/redux/actions/productActions.js
@@ -4,14 +4,14 @@ import axios from 'axios'
 export const getProducts = () => async (dispatch) => {
   try {
     dispatch({ type: actionTypes.GET_PRODUCTS_REQUEST })
-    const { data } = await axios.get("api/products")
+    const { data } = await axios.get("/api/products")
     dispatch({
       type: actionTypes.GET_PRODUCTS_SUCCESS,
       payload: data
     })
   } catch (error) {
     dispatch({
-      type: actionTypes.GET_PRODUCT_FAIL,
+      type: actionTypes.GET_PRODUCTS_FAIL,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message
     })
   }
@@ -20,7 +20,7 @@ export const getProducts = () => async (dispatch) => {
 export const getProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_REQUEST })
-    const { data } = await axios.get(`api/products/${id}`)
+    const { data } = await axios.get(`/api/products/${id}`)
     dispatch({
       type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS,
       payload: data
