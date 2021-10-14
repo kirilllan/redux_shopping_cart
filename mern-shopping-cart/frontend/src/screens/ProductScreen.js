cea7abb2af9f7eade452cfe64555181b63838288
kirilllan <s2100138@edu.bc.fi>
2021-10-14 12:41:58 +0300

    dynamic details in ProductScreen

--- mern-shopping-cart/frontend/src/screens/ProductScreen.js
+++ mern-shopping-cart/frontend/src/screens/ProductScreen.js
@@ -20,26 +20,34 @@ function ProductScreen({ match, history }) {
 
   return (
     <div className="productscreen">
-      <div className="productscreen__left">
-        <div className="left__image">
-          <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="product name" />
-        </div>
-        <div className="left__info">
-          <p className="left__name">Product 1</p>
-          <p>Price: $599.99</p>
-          <p>Decription: lalalala this is a description here yes it is, here it is, hehe</p>
-        </div>
-      </div>
-      <div className="productscreen__right">
-        <div ckassName="right__info">
-          <p>Price: <span>$499.99</span></p>
-          <p>Status:<span>In stock</span></p>
-          <p>Quantity:<select>
-            <option value="1">1</option>
-          </select></p>
-          <p><button type="button">add to cart</button></p>
-        </div>
-      </div>
+      {loading ? <h2>Loading...</h2> : error ? <h2>{error}</h2> :
+        <>
+          <div className="productscreen__left">
+            <div className="left__image">
+              <img src={product.imageUrl} alt={product.name} />
+            </div>
+            <div className="left__info">
+              <p className="left__name">{product.name}</p>
+              <p>Price: ${product.price}</p>
+              <p>Decription: {product.description}</p>
+            </div>
+          </div>
+          <div className="productscreen__right">
+            <div ckassName="right__info">
+              <p>Price: <span>${product.price}</span></p>
+              <p>Status:<span>{product.countInStock > 0 ? "In stock" : "Out of stock"}</span></p>
+              <p>
+                Quantity:
+                <select value={qty} onChange={e => setQty(e.target.value)}>
+                  {[...Array(product.countInStock).keys()].map(k => (<option value={x + 1} key={x + 1}>{x + 1}</option>))}
+                  <option value="1">1</option>
+                </select></p>
+              <p><button type="button">add to cart</button></p>
+            </div>
+          </div>
+        </>
+      }
+
     </div>
   )
 }
