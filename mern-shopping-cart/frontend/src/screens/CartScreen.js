97d5b72052740955509cb079c8675232eac7b2a8
kirilllan <s2100138@edu.bc.fi>
2021-10-16 12:47:30 +0300

    dynamic amount of CartItems

--- mern-shopping-cart/frontend/src/screens/CartScreen.js
+++ mern-shopping-cart/frontend/src/screens/CartScreen.js
@@ -1,15 +1,22 @@
 import './CartScreen.css'
 import CartItem from '../components/CartItem'
-import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 function CartScreen() {
+
+  const dispatch = useDispatch()
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
+
   return (
     <div className="cartscreen">
       <div className="cartscreen__left">
         <h2>Shopping Cart</h2>
-
+        {cartItems.length === 0 ? (
+          <div> Your cart is empty <Link to="/">Go back</Link></div>
+        ) : cartItems.map(item => <CartItem />)
+        }
       </div>
       <div className="cartscreen__right">
         <div className="cartscreen__info">
