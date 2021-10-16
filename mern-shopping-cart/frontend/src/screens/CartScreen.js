26b4a192a30dceec0b97cec0f54a401edd3540db
kirilllan <s2100138@edu.bc.fi>
2021-10-16 12:42:34 +0300

    imports in CartScreen

--- mern-shopping-cart/frontend/src/screens/CartScreen.js
+++ mern-shopping-cart/frontend/src/screens/CartScreen.js
@@ -1,13 +1,15 @@
 import './CartScreen.css'
 import CartItem from '../components/CartItem'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 function CartScreen() {
   return (
     <div className="cartscreen">
       <div className="cartscreen__left">
         <h2>Shopping Cart</h2>
-        <CartItem />
-        <CartItem />
+
       </div>
       <div className="cartscreen__right">
         <div className="cartscreen__info">
