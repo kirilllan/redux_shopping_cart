97e3592b1c59b7f7ed840614bf0ac7dd952bba51
kirilllan <s2100138@edu.bc.fi>
2021-10-17 13:16:26 +0300

    create qtyChangeHandler disatcher

--- mern-shopping-cart/frontend/src/screens/CartScreen.js
+++ mern-shopping-cart/frontend/src/screens/CartScreen.js
@@ -2,6 +2,7 @@ import './CartScreen.css'
 import CartItem from '../components/CartItem'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { addToCart } from '../redux/actions/cartActions'
 
 function CartScreen() {
 
@@ -9,13 +10,17 @@ function CartScreen() {
   const cart = useSelector(state => state.cart)
   const { cartItems } = cart
 
+  const qtyChangeHandler = (id, qty) => {
+    dispatch(addToCart(id, qty))
+  }
+
   return (
     <div className="cartscreen">
       <div className="cartscreen__left">
         <h2>Shopping Cart</h2>
         {cartItems.length === 0 ? (
           <div> Your cart is empty <Link to="/">Go back</Link></div>
-        ) : cartItems.map(item => <CartItem item={item} />)
+        ) : cartItems.map(item => <CartItem item={item} qtyChangeHandler={qtyChangeHandler} />)
         }
       </div>
       <div className="cartscreen__right">
