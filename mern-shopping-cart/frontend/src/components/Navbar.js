9f5e9f9f135db11baa2a38ba841bf1eedf5e70d8
kirilllan <s2100138@edu.bc.fi>
2021-10-18 11:43:51 +0300

    dynamic amount of cartItems in Navbar

--- mern-shopping-cart/frontend/src/components/Navbar.js
+++ mern-shopping-cart/frontend/src/components/Navbar.js
@@ -1,7 +1,14 @@
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function Navbar({ click }) {
+
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
+
+  const getCartCount = () => cartItems.reduce((qty, item) => qty + +item.qty, 0)
+
   return (
     <div className="navbar">
       <div className="navbar__logo">
@@ -13,7 +20,7 @@ function Navbar({ click }) {
             <i className="fas fa-shopping-cart" />
             <span>
               Cart
-              <span className="cartlogo__badge">0</span>
+              <span className="cartlogo__badge">{getCartCount()}</span>
             </span>
           </Link>
         </li>
